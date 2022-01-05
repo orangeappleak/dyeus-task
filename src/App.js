5a6d76f05220102ca6b3f1c03d90b4bb38938bbd
orangeappleak <appannagarikarthik.123@gmail.com>
2022-01-05 11:01:50 +0530

    basic layout designed, no styling yet

--- src/App.js
+++ src/App.js
@@ -3,11 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="dyeus-app">/
+    <section className="dyeus-app">/
       <div id="main-page">
+        <div id="main-page-image-wrapper">
+          <img src="https://images.unsplash.com/photo-1611744669444-5edf2ce1dd56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBhY2thZ2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="cannot display the image" />
+        </div>
+      
+      
+        <div id="product-page-content">
+          <div id="product-brief-info">
+            <h1>Everyday rinse and reload.</h1>
+            <p>Achieve this dream skin goal with this complete package of skincare products that will rejuvenate your day.</p>
+            <div id="product-ratings-reviews">
+              <div id="rating-stars">
 
+              </div>
+              <div id='rating-reviews'>
+                <h1>4.5</h1>
+                <p>click to read reviews.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      <div id="product-pricing">
+        <div id="product-price-value">
+          <h1>$899</h1>
+          <h1>$1299</h1>
+        </div>
+        <div id="product-monthly-pack-pricing">
+          <div id="1-month-pack">
+
+          </div>
+          <div id="3-month-pack"></div>
+        </div>
       </div>
-    </div>
+      </div>
+    </section>
   );
 }
 
